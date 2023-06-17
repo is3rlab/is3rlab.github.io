bf10b24844085fe1af09ae6efec4789114e5b75e
is3rlab <136397202+is3rlab@users.noreply.github.com>
2023-06-17 13:19:30 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,6 +14,26 @@
 			directionNav: false,
 			controlNav: true
 		});
+		
+		var map = $(".map");
+		var latitude = map.data("latitude");
+		var longitude = map.data("longitude");
+		if( map.length ){
+			
+			map.gmap3({
+				map:{
+					options:{
+						center: [latitude,longitude],
+						zoom: 15,
+						scrollwheel: false
+					}
+				},
+				marker:{
+					latLng: [latitude,longitude],
+				}
+			});
+			
+		}
 	});
 
 	$(window).load(function(){
